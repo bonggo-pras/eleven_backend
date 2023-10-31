3bc692b65e0c659bed7754be8b19a2b3324ab7cc
Bonggo Prasetyanto <userpras@gmail.com>
2023-10-31 10:44:05 +0700

    add: payment method qris

--- packages/Webkul/Midtrans/src/Config/paymentmethods.php
+++ packages/Webkul/Midtrans/src/Config/paymentmethods.php
@@ -4,9 +4,17 @@ return [
     'midtrans_bank_transfer' => [
         'code' => 'midtrans.bank_transfer',
         'title' => 'ATM/Bank Transfer',
-        'description' => 'Bayar dengan ATM atau Transfer Bank',
+        'description' => 'Bayar dengan ATM atau Transfer Bank (BCA atau BRI)',
         'class' => 'Webkul\Midtrans\Payment\BankTransfer',
         'active' => true,
         'sort' => 1
+    ],
+    'midtrans_e_wallet' => [
+        'code' => 'midtrans.e_wallet',
+        'title' => 'Dompet Digital',
+        'description' => 'Bayar dengan Dompet Digital (Qris)',
+        'class' => 'Webkul\Midtrans\Payment\EWallet',
+        'active' => true,
+        'sort' => 2
     ],
 ];
